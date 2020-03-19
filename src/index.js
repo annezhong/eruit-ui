42a4dd2756b1a3984b8d91bfc581d1693d83ce7e
小英 <32158733@qq.com>
2020-03-19 12:14:47 +0800

    更改export方式

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import leftright from "./components/leftright/leftright.vue"
 import login from "./components/login/login.vue"
 import manageLayout from "./components/manageLayout/mainlayout.vue"
 import newslist from "./components/newslist/list.vue"
+function install(Vue) {
+    var components = [button, gridtree, leftright, login, manageLayout, newslist];
+    components.map(component => {
+        Vue.component(component.name, component);
+    })
+}
 
-
-export default {
-    install: function (Vue) {
-        var components = [button, gridtree, leftright, login, manageLayout, newslist];
-        components.map(component => {
-            Vue.component(component.name, component);
-        })
-    },
+export {
+    install,
     button,
     gridtree,
     leftright,
